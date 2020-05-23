9a431bfd06b376987efc79a3d1b28fb23244c7cc
Renzo Oldani <oldanirenzo@gmail.com>
2020-05-23 08:56:03 -0300

    Agregado el eliminar un producto con 1 boton

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,34 +1,63 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class CardList extends Component {
+    _isMounted = false;
     constructor(props) {
         super(props)
 
         this.state = {
-
+            id: ''
         }
     }
 
+    componentDidMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false
+    }
+
+    borrar = (e) => {
+
+        axios({
+            method: 'DELETE',
+            url: require = ('http://localhost:9001/api/v1/instrumento/delete/' + this.props.id)
+        })
+            .then(res => {
+                console.log(res)
+            })
+        document.getElementById("tarjetaProductos").reset();
+            window.location.replace('')
+
+    }
+
+
     render() {
         return (
-
-            <div className="card border-dark mb-3">
-                <div className="row no-gutters">
-                    <div className="col-md-2" >
-                        <a href={"/detalleProducto/" + this.props.id}>  <img src={this.props.imagen} className="card-img" alt={this.props.descripcion} /> </a>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="card-body">
-                            <h2 className="card-title">{this.props.nombre}</h2>
-                            <br />
-                            <h3 className="card-text">$ {this.props.precio}</h3>
-                            <p className="card-text" className="text-danger">{this.props.costoEnvio}</p>
-                            <p className="card-text"><small className="text-muted">{this.props.cantidadVendida} vendidos</small></p>
+            <form id="tarjetaProductos">
+                <div className="card border-dark mb-3">
+                    <div className="row no-gutters" >
+                        <div className="col-md-2" >
+                            <a href={"/detalleProducto/" + this.props.id}>  <img src={this.props.imagen} className="card-img" alt={this.props.descripcion} style={{ width: "400px" }} /> </a>
+                        </div>
+                        <div className="col-md-5">
+                            <div className="card-body text-center">
+                                <h2 className="card-title"><b>{this.props.nombre}</b></h2>
+                                <br />
+                                <h3 className="card-text">$ {this.props.precio}</h3>
+                                <p className="card-text" className="text-danger">{this.props.costoEnvio}</p>
+                                <p className="card-text"><small className="text-muted">{this.props.cantidadVendida} vendidos</small></p>
+                                <div className="btn-group-vertical">
+                                    <button type="button" className="btn btn-danger" onClick={this.borrar}>Borrar</button>
+                                    <button type="button" className="btn btn-primary" >Modificar</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        )
+                </div >
+            </form>)
     }
 }
 
