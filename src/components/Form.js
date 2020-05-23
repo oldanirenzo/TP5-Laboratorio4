30fe95b1b53851b67ac19717401058fa1d145c8a
Renzo Oldani <oldanirenzo@gmail.com>
2020-05-23 14:32:38 -0300

    Agrego comentarios al codigo

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,8 +13,7 @@ class Form extends Component {
             precio: '',
             costo: '',
             cantidadVendida: '',
-            imagen: null,
-            key: 'reset'
+            imagen: null
         }
     }
 
@@ -33,14 +32,17 @@ class Form extends Component {
                 cantidadVendida: res.data.cantidadVendida,
                 descripcion: res.data.descripcion,
                 edit: true,
-                _id: this.props.id
+                id: this.props.id
             })
         }
     }
 
     submitHandler = () => {
 
+        // Si edit es verdadero, actualiza, sino, agrega
         if (this.state.edit) {
+
+            // Actualizo el producto perteneciente al id que mando
             axios({
                 method: 'PUT',
                 url: 'http://localhost:9001/api/v1/instrumento/put/' + this.props.match.params.id,
@@ -113,21 +115,23 @@ class Form extends Component {
                     this.setState({ imagen: null })
                 })
                 .then(() => {
+                    // Vuelvo al formulario, asi se limpian los campos
                     window.location.href = "/formulario"
                 })
         }
 
 
     }
-
+    // Agrego los atributos que manda el usuario
     handleInputChange = e => {
         this.setState({
             [e.target.name]: e.target.value
         });
     }
+    // Agrego los archivos de imagen
     imagenChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.files[0]
+            [e.target.name]: e.target.files[0] //Es un array, y me traigo el primero, el cual yo cargo
         });
     }
 
@@ -140,22 +144,44 @@ class Form extends Component {
                     <form onSubmit={this.submitHandler} className="form-group" encType="multipart/form-data" id="formNuevo">
                         <label className="col-form-label col-form-label-sm">Instrumento:</label>
                         <input className="form-control" type="text" placeholder="Nombre producto" name="instrumento" id="instrumento" defaultValue={this.state.instrumento} onChange={this.handleInputChange}></input>
-                        <br /><label className="col-form-label col-form-label-sm">Descripcion:</label>
+
+                        <br />
+
+                        <label className="col-form-label col-form-label-sm">Descripcion:</label>
                         <textarea className="form-control" type="text" placeholder="Descripcion producto" name="descripcion" id="descripcion" defaultValue={this.state.descripcion} onChange={this.handleInputChange}></textarea>
-                        <br /><label className="col-form-label col-form-label-sm">Marca:</label>
+
+                        <br />
+
+                        <label className="col-form-label col-form-label-sm">Marca:</label>
                         <input className="form-control" type="text" placeholder="Marca" name="marca" id="marca" defaultValue={this.state.marca} onChange={this.handleInputChange}></input>
-                        <br /><label className="col-form-label col-form-label-sm">Modelo:</label>
+
+                        <br />
+
+                        <label className="col-form-label col-form-label-sm">Modelo:</label>
                         <input className="form-control" type="text" placeholder="Modelo" name="modelo" id="modelo" defaultValue={this.state.modelo} onChange={this.handleInputChange}></input>
-                        <br /><label className="col-form-label col-form-label-sm">Precio del producto:</label>
+
+                        <br />
+
+                        <label className="col-form-label col-form-label-sm">Precio del producto:</label>
                         <input className="form-control" type="number" placeholder="Precio producto" name="precio" id="precio" defaultValue={this.state.precio} onChange={this.handleInputChange}></input>
-                        <br /><label className="col-form-label col-form-label-sm">Costo de envio:</label>
+
+                        <br />
+
+                        <label className="col-form-label col-form-label-sm">Costo de envio:</label>
                         <input className="form-control" type="number" placeholder="Costo de envio" name="costo" id="costo" defaultValue={this.state.costo} onChange={this.handleInputChange}></input>
-                        <br /><label className="col-form-label col-form-label-sm">Cantidad vendida:</label>
+
+                        <br />
+
+                        <label className="col-form-label col-form-label-sm">Cantidad vendida:</label>
                         <input className="form-control" type="number" placeholder="Cantidad vendidos" name="cantidadVendida" id="cantidadVendida" defaultValue={this.state.cantidadVendida} onChange={this.handleInputChange}></input>
+
                         <br />
+
                         <input type="file" className="form-control-file" name="imagen" id="imagen" onChange={this.imagenChange} required />
+
                         <br />
-                        <button type="submit" className="btn btn-primary">Submit</button>
+
+                        <button type="submit" className="btn btn-primary">Guardar</button>
                     </form >
                 </div>
             </React.Fragment>
